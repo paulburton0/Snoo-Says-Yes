7e478eadbc40b93348a137978f915a7aa0707aa6
paulburton0 <Paulburton0@gmail.com>
2011-05-03 11:14:22 -0400

    Separated styles from options.html and popup.html into stylesheet.css
    Added close button on popup.

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
-var     url = chrome.extension.getBackgroundPage().selectedURL;
-var   title = chrome.extension.getBackgroundPage().selectedTitle;
+var url = chrome.extension.getBackgroundPage().selectedURL;
+var title = chrome.extension.getBackgroundPage().selectedTitle;
 
 url = encodeURIComponent(url);
 var submitUrl = 'http://www.reddit.com/submit?url=' + url;
@@ -11,9 +11,16 @@ var dateNow = new Date().getTime();
 var entryDate; 
 var oneDay = 86400000; // milliseconds
 
+$(document).ready(function(){
+    // Close the popup when the close button is clicked
+    $('#close').click(function(){
+        closePopup();
+    });
+});
+
 // get URL info
-function getURLInfo(url)
-{
+function getURLInfo(url){
+
     var redditUrl = 'http://www.reddit.com/api/info.json?url=' +url;
     $.getJSON(
         redditUrl,
@@ -22,8 +29,7 @@ function getURLInfo(url)
 }
 
 // parse json data
-function parseURLData(jsonData)
-{
+function parseURLData(jsonData){
     submitCount=0;
     $('div#timeout').hide(0);
     var data = [ 0, "", "", "", "", "", "", "", "", url];
@@ -42,14 +48,26 @@ function parseURLData(jsonData)
     }
     
     if(submitCount) {
+        
+        var submissionLabel;
+
+        if(submitCount == 1){
+            submissionLabel = 'time';
+        }
+
+        else{
+            submissionLabel = 'times';
+        }
+
         $('#data').append('<h4>' + title + '</h4>');
         $('#data').append('<div id="submission_status"><span id="submitnumber">submitted ' +
-                submitCount + ' times | </span> <a target="_blank" title="Post to reddit"' +
+            submitCount + ' ' + submissionLabel + ' | </span> <a target="_blank" title="Post to reddit"' +
             ' href="' + resubmitUrl + 
             '">resubmit</a></div>');
         
         showLinks();
     }
+
     else {
         if(localStorage["hideStatus"] == 'hide'){
             $('#data').append('<h4>' + title + '</h4>');
@@ -61,7 +79,6 @@ function parseURLData(jsonData)
                 url: submitUrl
             });
 
-            //closePopup();
             window.close();
         }
     }
@@ -109,4 +126,3 @@ function parseAge(entryDateMs){
 
     return submitted;
 }
-
